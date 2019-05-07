8f6d348a2e0ba8f504ce69c96fa3459e01b2eb91
Michael Cain <michaelcaincb@gmail.com>
2019-05-07 20:14:16 +0000

    seperated everything into modular file settup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import MessageList from "./MessageList.jsx";
+import ChatBar from "./ChatBar.jsx";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: { name: "?" }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Mike",
+          content: "Has anyone seen my marbles?"
+        },
+        {
+          username: "Anonymous",
+          content: "No, I HAVE NOTTTT!!!!."
+        }
+      ]
+    };
+  }
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList />
+        <ChatBar currentUser={this.state.currentUser.name} />
+      </div>
     );
   }
 }
+
 export default App;
---
+++ src/MessageList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import Messages from "./Messages.jsx";
+
+class MessageList extends Component {
+  render() {
+    return <Messages />;
+  }
+}
+export default MessageList;
